19593865e57eb6286749c3f33fde5af0e5ce27d6
Steve Ngobami <steve.ngobami@gmail.com>
2022-07-25 22:03:58 +0200

    adding app and config files

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ class DefaultConfig:
     """Configuration for the bot."""
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
-    LUIS_APP_ID = os.environ.get("LuisAppId", "")
-    LUIS_API_KEY = os.environ.get("LuisAPIKey", "")
+    # APP_ID = os.environ.get("MicrosoftAppId", "")
+    # APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    LUIS_APP_ID = os.environ.get("LUIS_APPID", "")
+    LUIS_API_KEY = os.environ.get("AUTH_LUIS", "")
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
-    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "")
+    LUIS_API_HOST_NAME = os.environ.get("AUTH_ENDPOINT", "")
     #APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
         #"AppInsightsInstrumentationKey", ""
     #)
